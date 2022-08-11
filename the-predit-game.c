2c4ca5ff6e33687ffcb9e24d2d60b35d368c37a4
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2022-08-11 18:14:31 +0530

    Update
    #include<stdio.h>
    #include<stdlib.h>
    #include<time.h>
    
    int main(){
        int number, guess, nguesses=1;
        srand(time(0));
        number = rand()%100+1; //Generates a random number between 1 and 100
        // printf("The number is %d\n", number);
        // Keep running the loop until the number is guessed
        printf("%d\n",number);
        do{
            printf("Guess the number between 1 to 100\n");
            scanf("%d",&guess);
            if(guess>number){
                printf("Lower number please!\n");
            } 
            else if(guess<number){
                printf("Higher number please!\n");
            }
            else{
                printf("You guessed it in %d attempts\n");
            }
            nguesses++;
        }while(guess!=number);    
        return 0; 
    }

--- the-predit-game.c
+++ the-predit-game.c
@@ -3,9 +3,25 @@
 #include<time.h>
 
 int main(){
-    int number;
+    int number, guess, nguesses=1;
     srand(time(0));
     number = rand()%100+1; //Generates a random number between 1 and 100
-    printf("The number is %d.\n", number);
-    return 0;
-}
+    // printf("The number is %d\n", number);
+    // Keep running the loop until the number is guessed
+    printf("%d\n",number);
+    do{
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d",&guess);
+        if(guess>number){
+            printf("Lower number please!\n");
+        } 
+        else if(guess<number){
+            printf("Higher number please!\n");
+        }
+        else{
+            printf("You guessed it in %d attempts\n");
+        }
+        nguesses++;
+    }while(guess!=number);    
+    return 0; 
+} 
